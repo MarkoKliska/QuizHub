ad97351a6fa1496a768eece498bfdde5acbe844e
MarkoKliska <021markokliska@gmail.com>
2025-09-24 14:10:05 +0200

    Initial commit

---
+++ backend/QuizHub/src/QuizHub.Api/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizHub.Api.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration config)
+    {
+        return services;
+    }
+}
